9cedcf083ebd43b38b436992ca34a315c2fe1098
Hamim <155163033+hamim0047@users.noreply.github.com>
2026-05-09 23:14:45 +0600

    Delete routes directory

--- routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { handleHeartAssistantChat } = require("../controllers/chatController");
-const { authenticate } = require("../middlewares/authMiddleware");
-
-router.post("/heart-assistant", authenticate, handleHeartAssistantChat);
-
-module.exports = router;
